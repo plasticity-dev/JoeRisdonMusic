8f9f6df2beade4fb2cab30f6101c9b95e55cfbe8
Sean Norton <Norton.Sean.M@gmail.com>
2017-02-28 21:22:42 -0600

    add admin login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    get 'admin', to: 'devise/sessions#new'
+    get 'admin/edit', to: 'devise/registrations#edit'
+  end
+
+  devise_for :users, :controllers => {:registrations => "users/registrations"}
+
   resources :gigs
   resources :albums
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
