8256c8303b36123c023e0bab5187ce82b0179a57
Sean Norton <Norton.Sean.M@gmail.com>
2017-03-04 14:25:08 -0600

    add past and future shows and reorder shows on index

--- app/models/gig.rb
+++ app/models/gig.rb
@@ -4,4 +4,24 @@ class Gig < ApplicationRecord
     self.venue.gsub('&', 'and')
   end
 
+  def self.future_dates
+    future_gigs = []
+    Gig.all.each do |gig|
+      if gig.date.future?
+        future_gigs.push(gig)
+      end
+    end
+    return future_gigs.reverse()
+  end
+
+  def self.past_dates
+    past_gigs = []
+    Gig.all.each do |gig|
+      if gig.date.past?
+        past_gigs.push(gig)
+      end
+    end
+    return past_gigs
+  end
+
 end
